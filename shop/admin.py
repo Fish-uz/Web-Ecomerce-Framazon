4e112d94b3a4964992723704ca06794fb46ac740
FrankLaptopAri <frankuzcategui22@gmail.com>
2026-03-25 15:37:11 -0400

    Admin registrado, superusuario creado y lógica de carrito profesional añadida

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} # Esto escribe el slug solo mientras escribes el nombre
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'vendedor', 'created']
+    list_filter = ['available', 'created']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
