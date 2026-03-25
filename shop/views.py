796f9a55b84742fc322ac8d89355644b00e3efe1
FrankLaptopAri <frankuzcategui22@gmail.com>
2026-03-25 16:16:56 -0400

    Solucion y orden URL. Carga de archivos media para imagenes

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product
+from django.views.decorators.http import require_POST
+from .cart import Cart
 
-# Create your views here.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get('quantity', 1))
+    cart.add(product=product, quantity=quantity)
+    return redirect('shop:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'shop/cart/detail.html', {'cart': cart})
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('shop:cart_detail')
